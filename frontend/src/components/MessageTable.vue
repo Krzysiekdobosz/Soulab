8627ebe5ebce010830514e355a6fbd1b342ccf0f
Krzysztof <krzysiekdobosz2001@gmail.com>
2024-09-22 02:29:20 +0200

    Update MessageTable.vue

--- frontend/src/components/MessageTable.vue
+++ frontend/src/components/MessageTable.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="overflow-x-auto">
-    <table v-if="messages.length" class="min-w-full bg-white">
+    <table v-if="messages.length" class="min-w-full bg-white border border-gray-300">
       <thead class="bg-gray-800 text-white">
         <tr>
-          <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm">Imię</th>
-          <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm">Nazwisko</th>
-          <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm">E-mail</th>
-          <th class="w-1/3 py-3 px-4 uppercase font-semibold text-sm">Wiadomość</th>
-          <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm">Data</th>
+          <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm border-b border-gray-300">Imię</th>
+          <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm border-b border-gray-300">Nazwisko</th>
+          <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm border-b border-gray-300">E-mail</th>
+          <th class="w-1/3 py-3 px-4 uppercase font-semibold text-sm border-b border-gray-300">Wiadomość</th>
+          <th class="w-1/6 py-3 px-4 uppercase font-semibold text-sm border-b border-gray-300">Data</th>
         </tr>
       </thead>
       <tbody class="text-gray-700">
-        <tr v-for="(message, index) in messages" :key="index">
-          <td class="w-1/6 py-3 px-4">{{ message.firstName }}</td>
-          <td class="w-1/6 py-3 px-4">{{ message.lastName }}</td>
-          <td class="w-1/6 py-3 px-4">{{ message.email }}</td>
-          <td class="w-1/3 py-3 px-4">{{ message.message }}</td>
+        <tr v-for="(message, index) in messages" :key="index" class="border-b border-gray-300">
+          <td class="w-1/6 py-3 px-4 border-r border-gray-300">{{ message.firstName }}</td>
+          <td class="w-1/6 py-3 px-4 border-r border-gray-300">{{ message.lastName }}</td>
+          <td class="w-1/6 py-3 px-4 border-r border-gray-300">{{ message.email }}</td>
+          <td class="w-1/3 py-3 px-4 border-r border-gray-300">{{ message.message }}</td>
           <td class="w-1/6 py-3 px-4">{{ message.createdAt }}</td>
         </tr>
       </tbody>
